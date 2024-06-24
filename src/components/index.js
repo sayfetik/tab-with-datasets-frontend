85cfa32ece66ebb3830fe2a81ad4fa5006f5f57e
sayfetik <sayfetik2005@gmail.com>
2024-06-24 22:08:58 +0300

    create-node-modules

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header} from './Header/Header';
-export { default as Subheader} from './Subheader/Subheader';
-export { default as Upload} from './Upload/Upload'
-export { default as Back} from './Back/Back'
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as ReactDOMClient from 'react-dom/client'
-import App from './App';
-import './css/main.css'
-
-const app = ReactDOMClient.createRoot(document.getElementById('root'));
-app.render(<App />);
-
-/*ReactDOM.render(<App />, document.getElementById('root));*/
