86473f082d67bddd0214c0dd9b815af85c48cc31
sayfetik <sayfetik2005@gmail.com>
2024-06-30 14:00:57 +0300

    update-frontend

--- src/components/DatasetCard/DatasetCard.js
+++ src/components/DatasetCard/DatasetCard.js
@@ -6,17 +6,17 @@ import { useNavigate } from 'react-router-dom';
 const DatasetCard = () => {
     const navigate = useNavigate();
     return (
-        <div id='datasetCard'>
-        <div onClick={()=>{navigate('/dataset')}} id='card' >
-            <img id='datasetImage' src={datasetImage}></img>
-            <h3>Вставить название датасета</h3>
-            <div id='cardInfo'>
-                <p className='fieldInfo'>Вставить автора</p>
-                <p className='fieldInfo'>Вставить дату обновления</p>
-                <p className='fieldInfo'>Вставить количество скачиваний</p>
-                <p className='fieldInfo'>2 файла (CSV) 9 МБ</p>
+        <div onClick={()=>{navigate('/dataset')}} id='datasetCard'>
+            <div>
+                <img id='datasetImage' src={datasetImage}></img>
+                <div id='nameOfDataset'>Вставить название датасета</div>
+                <div id='cardInfo'>
+                    <p className='fieldInfo'>Вставить автора</p>
+                    <p className='fieldInfo'>Вставить дату обновления</p>
+                    <p className='fieldInfo'>Вставить количество скачиваний</p>
+                    <p className='fieldInfo'>2 файла (CSV) 9 МБ</p>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
