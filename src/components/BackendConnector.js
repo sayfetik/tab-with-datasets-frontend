18750f374086f0bec1d900419a49e04b6d3d3606
sayfetik <sayfetik2005@gmail.com>
2024-11-09 13:44:55 +0300

    Fix upload endpoint

--- src/components/BackendConnector.js
+++ src/components/BackendConnector.js
@@ -164,7 +164,7 @@ export default class BackendConnector {
             const response = await axios({
                 method: 'post',
                 url: url,
-                data: data,
+                data: formData,
                 headers: {
                     'Content-Type': 'multipart/form-data',
                     'Authorization': `Bearer ${token}`
